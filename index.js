1dc9b7a84970eee20defc115e3896011ba212bcc
Lauraklijn <lauraklijn12@hotmail.com>
2020-03-31 15:48:44 +0200

    Express server setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4050;
+
+const loggerMiddleWare = require("morgan");
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/echo", (req, res) => {
+  res.json({
+    youPosted: {
+      ...req.body
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
