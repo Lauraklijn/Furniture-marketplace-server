ead72f6609ba084947b8ecb7eba01bec13f6b4f6
Lauraklijn <lauraklijn12@hotmail.com>
2020-03-31 15:15:32 +0200

    Add user model & migration, and started seed

---
+++ seeders/20200331125528-some-users.js
@@ -0,0 +1,22 @@
+// 'use strict';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//    return queryInterface.bulkInsert(
+//      "users",
+//      [{
+//        name: "test user",
+//        email: "user@test.nl",
+//        password: 123,
+//        createdAt: new Date(),
+//        updatedAt: new Date(),
+//      }]
+//    )
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+
+//       return queryInterface.bulkDelete('users', null, {});
+
+//   }
+// };
