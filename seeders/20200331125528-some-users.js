87b048bbb346e9508f888f590a9fb3646c62d817
Lauraklijn <lauraklijn12@hotmail.com>
2020-03-31 15:25:44 +0200

    Add seed for users, password with bcrypt

--- seeders/20200331125528-some-users.js
+++ seeders/20200331125528-some-users.js
@@ -1,22 +1,34 @@
-// 'use strict';
+"use strict";
+const bcrypt = require("bcrypt");
 
-// module.exports = {
-//   up: (queryInterface, Sequelize) => {
-//    return queryInterface.bulkInsert(
-//      "users",
-//      [{
-//        name: "test user",
-//        email: "user@test.nl",
-//        password: 123,
-//        createdAt: new Date(),
-//        updatedAt: new Date(),
-//      }]
-//    )
-//   },
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const saltRounds = 10;
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "testuser",
+          email: "user@test.nl",
+          password: bcrypt.hashSync("test1234", saltRounds),
+          imageUrl: "testimage",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "testuser2",
+          email: "user2@test.nl",
+          password: bcrypt.hashSync("test1234", saltRounds),
+          imageUrl: "testimage",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
 
-//   down: (queryInterface, Sequelize) => {
-
-//       return queryInterface.bulkDelete('users', null, {});
-
-//   }
-// };
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
